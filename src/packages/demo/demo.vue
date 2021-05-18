b843b0299478ea1ab0b70341c9b6a33eff4c1ac0
liuyonghui <liuyonghui@jetair.com.cn>
2021-05-18 17:16:55 +0800

    chore: 添加 dome 模板

---
+++ src/packages/demo/demo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="demo">
+    <h2>基础用法</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "demo-icon",
+  data() {
+    return {};
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
